4b358f9fb7d964767d3806fbe1c57af7a2c9ba20
sPalominoSal <82245130+fiddle-leaf@users.noreply.github.com>
2023-06-15 21:25:31 -0400

    finished Manipulating the DOM exercises

--- script.js
+++ script.js
@@ -17,31 +17,78 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+  let pageTitle = document.getElementById("main-title");
+  pageTitle.innerText = "Welcome to my homepage!"
 
   // Part 2
-
+  let pageBody = document.getElementsByTagName("body");
+  pageBody[0].style.background = "hsl(305, 100%, 95%)";
 
   // Part 3
-
+  let favoriteThingsEl = document.getElementById("favorite-things");
+  //console.log(favoriteThingsList);
+  favoriteThingsEl.removeChild(favoriteThingsEl.lastElementChild);
+  console.log(favoriteThingsEl);
 
   // Part 4
+  let specialTitleEl = document.querySelectorAll(".special-title");
 
+  specialTitleEl.forEach(title => {
+    title.style.fontSize = "2em";
+  })
 
   // Part 5
+  const pastRacesEl = document.getElementById("past-races")
+  console.log(pastRacesEl);
 
+  const raceCities = pastRacesEl.querySelectorAll("li");
+  console.log(raceCities);
+
+  raceCities.forEach(city =>{
+    if (city.textContent.toLowerCase() == "chicago") {
+      pastRacesEl.removeChild(city);
+    } else {
+      console.log("false!");
+    }
+})
 
   // Part 6
-
+  let newLi = document.createElement("li");
+  newLi.textContent = "Lima";
+  pastRacesEl.appendChild(newLi);
 
   // Part 7
+  let blogPostEl = document.createElement("div");
+  blogPostEl.classList.add("blog-post", "purple");
 
+  let blogTitleEl = document.createElement("h1");
+  blogTitleEl.textContent = "Lima";
+
+  let blogBody = document.createElement("p");
+  blogBody.textContent = "I got here by plane!".toUpperCase();
+
+  blogPostEl.append(blogTitleEl, blogBody);
+
+  const blogPosts = document.querySelectorAll(".blog-post");
+  console.log(blogPosts);
+
+  document.querySelector(".main").append(blogPostEl);
 
   // Part 8
-
+  let quoteTitle = document.querySelector("#quote-title");
+  quoteTitle.addEventListener("click", randomQuote(event));
 
   // Part 9
+  blogPosts.forEach(post => {
 
+    post.addEventListener("mouseout", event =>{
+      post.classList.toggle("purple");
+    });
+
+    post.addEventListener("mouseenter", event =>{
+      post.classList.toggle("red");
+    })
+  })
 
 
 
